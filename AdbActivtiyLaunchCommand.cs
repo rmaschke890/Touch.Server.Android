eb720ad930eab87bf293c8955d474a27a9b17280
Maschke <rico.maschke@sage.com>
2017-03-17 14:52:57 +0100

    Pass parameter to the app, which came from the caller instance.

--- AdbActivtiyLaunchCommand.cs
+++ AdbActivtiyLaunchCommand.cs
@@ -1,68 +1,82 @@
-﻿namespace Touch.Server.Android
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Touch.Server.Android
 {
-  using System.Diagnostics;
+    using System.Diagnostics;
 
-  class AdbActivtiyLaunchCommand
-  {
-    private const string AdbExeName = "adb.exe";
-
-    public string AdbExeFile { get; set; }
-    public string Activity { get; set; }
-    public int ExitCode { get; set; }
-
-    public AdbActivtiyLaunchCommand(string activity, string adbExeFile)
+    class AdbActivtiyLaunchCommand
     {
-      this.Activity = activity;
-      this.AdbExeFile = this.PrepareAdbExeFilePath(adbExeFile);
-      this.ExitCode = -1;
+        private const string AdbExeName = "adb.exe";
+
+        public string AdbExeFile { get; set; }
+        public string Activity { get; set; }
+        public int ExitCode { get; set; }
+        public IList<string> AdbParams { get; set; }
+
+        public AdbActivtiyLaunchCommand(string activity, string adbExeFile)
+        {
+            this.Activity = activity;
+            this.AdbExeFile = this.PrepareAdbExeFilePath(adbExeFile);
+            this.ExitCode = -1;
+        }
+
+        public void Execute()
+        {
+            ServerRunner.LogMessage("Executing adb command\n Activity : {0}\n AdbExeFilePath : {1}", this.Activity, this.AdbExeFile);
+
+            var info = new ProcessStartInfo
+            {
+                FileName = this.AdbExeFile,
+                UseShellExecute = true,
+                Arguments = this.BuildActivitylaunchCommand(),
+            };
+
+            ServerRunner.LogMessage($"arguments for adb: {info.Arguments}");
+
+            using (var proc = new Process())
+            {
+                proc.StartInfo = info;
+
+                proc.Start();
+                proc.WaitForExit();
+
+                ServerRunner.LogMessage("Finished with adb command (return code : {0})", proc.ExitCode);
+                this.ExitCode = proc.ExitCode;
+            }
+        }
+
+        private string BuildActivitylaunchCommand()
+        {
+            var command = "shell am start -a android.intent.action.MAIN -n " + this.Activity;
+
+            if (AdbParams != null && AdbParams.Any())
+            {
+                foreach (var adbParam in AdbParams)
+                    command += $" {adbParam}";
+            }
+
+            return command;
+        }
+
+        private string PrepareAdbExeFilePath(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return AdbExeName;
+            }
+
+            if (path.Contains("adb"))
+            {
+                return path;
+            }
+
+            if (!path.EndsWith("\\"))
+            {
+                path += "\\";
+            }
+
+            return path + AdbExeName;
+        }
     }
-
-    public void Execute()
-    {
-      ServerRunner.LogMessage("Executing adb command\n Activity : {0}\n AdbExeFilePath : {1}", this.Activity, this.AdbExeFile);
-
-      var info = new ProcessStartInfo
-      {
-        FileName = this.AdbExeFile,
-        UseShellExecute = true,
-        Arguments = this.BuildActivitylaunchCommand(),
-      };
-
-      using (var proc = new Process())
-      {
-        proc.StartInfo = info;
-
-        proc.Start();
-        proc.WaitForExit();
-        
-        ServerRunner.LogMessage("Finished with adb command (return code : {0})", proc.ExitCode);
-        this.ExitCode = proc.ExitCode;
-      }
-    }
-
-    private string BuildActivitylaunchCommand()
-    {
-      return "shell am start -a android.intent.action.MAIN -n " + this.Activity;
-    }
-
-    private string PrepareAdbExeFilePath(string path)
-    {
-      if (string.IsNullOrWhiteSpace(path))
-      {
-        return AdbExeName;
-      }
-
-      if (path.Contains("adb"))
-      {
-        return path;
-      }
-
-      if (!path.EndsWith("\\"))
-      {
-        path += "\\";
-      }
-
-      return path + AdbExeName;
-    }
-  }
 }
--- ServerRunner.cs
+++ ServerRunner.cs
@@ -1,141 +1,145 @@
-﻿namespace Touch.Server.Android
+﻿using System.Linq;
+
+namespace Touch.Server.Android
 {
-  using System;
-  using System.Net;
-  using System.Threading;
+    using System;
+    using System.Net;
+    using System.Threading;
 
-  class ServerRunner
-  {
-    private static readonly CommandLineOptions Options = new CommandLineOptions();
-    private static SimpleConnectionListener _connectionListener;
-    private static AdbActivtiyLaunchCommand _adbActivtiyLaunchCommand;
-
-    public static int Main(string[] args)
+    class ServerRunner
     {
-      var returnCode = -1;
+        private static readonly CommandLineOptions Options = new CommandLineOptions();
+        private static SimpleConnectionListener _connectionListener;
+        private static AdbActivtiyLaunchCommand _adbActivtiyLaunchCommand;
 
-      if (CommandLine.Parser.Default.ParseArguments(args, Options))
-      {
-        if (!string.IsNullOrWhiteSpace(Options.Activity))
+        public static int Main(string[] args)
         {
-          returnCode = StartAdbAndListener();
-        }
-        else
-        {
-          StartListener();
-          returnCode = _connectionListener.ExitCode;
-        }
-      }
-      else
-      {
-        Console.WriteLine("Failed to parse options");
-      }
+            var returnCode = -1;
 
-      return returnCode;
+            if (CommandLine.Parser.Default.ParseArguments(args, Options))
+            {
+                if (!string.IsNullOrWhiteSpace(Options.Activity))
+                {
+                    returnCode = StartAdbAndListener();
+                }
+                else
+                {
+                    StartListener();
+                    returnCode = _connectionListener.ExitCode;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse options");
+            }
+
+            return returnCode;
+        }
+
+        private static void StartListener()
+        {
+            _connectionListener = PrepareListener();
+            _connectionListener.Initialize();
+            _connectionListener.Start();
+        }
+
+        private static int StartAdbAndListener()
+        {
+            var handles = new ManualResetEvent[2];
+
+            var listenerHandle = new ManualResetEvent(false);
+            Action listenerAction = () =>
+            {
+                try
+                {
+                    StartListener();
+                }
+                finally
+                {
+                    listenerHandle.Set();
+                }
+            };
+
+            handles[0] = listenerHandle;
+            ThreadPool.QueueUserWorkItem(x => listenerAction());
+
+            var adbHandle = new ManualResetEvent(false);
+            Action adbAction = () =>
+            {
+                try
+                {
+                    _adbActivtiyLaunchCommand = new AdbActivtiyLaunchCommand(Options.Activity, Options.AdbPath);
+                    if (Options.AdbParams != null && Options.AdbParams.Count > 0)
+                        _adbActivtiyLaunchCommand.AdbParams = Options.AdbParams;
+                    _adbActivtiyLaunchCommand.Execute();
+                }
+                finally
+                {
+                    adbHandle.Set();
+                }
+            };
+            handles[1] = adbHandle;
+            ThreadPool.QueueUserWorkItem(x => adbAction());
+
+            WaitHandle.WaitAll(handles);
+
+            if (_connectionListener != null && _adbActivtiyLaunchCommand != null)
+            {
+                if (_connectionListener.ExitCode == 0 && _adbActivtiyLaunchCommand.ExitCode == 0)
+                {
+                    return 0;
+                }
+            }
+
+            return -1;
+        }
+
+        public static void LogMessage(string message)
+        {
+            if (Options != null && Options.Verbose)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        public static void LogMessage(string message, params object[] objects)
+        {
+            if (Options != null && Options.Verbose)
+            {
+                Console.WriteLine(message, objects);
+            }
+        }
+
+        private static SimpleConnectionListener PrepareListener()
+        {
+            var listener = new SimpleConnectionListener();
+
+            IPAddress ip;
+            if (String.IsNullOrEmpty(Options.IpAddress) || !IPAddress.TryParse(Options.IpAddress, out ip))
+            {
+                LogMessage("Failed to parse ip address. Using : " + IPAddress.Any);
+                listener.Address = IPAddress.Any;
+            }
+            else
+            {
+                listener.Address = ip;
+            }
+
+            ushort p;
+            if (UInt16.TryParse(Options.Port, out p))
+            {
+                listener.Port = p;
+            }
+            else
+            {
+                Console.WriteLine("Failed to parse port. Will use dafult.");
+            }
+
+            listener.LogPath = Options.LogFilePath ?? ".";
+            listener.LogFile = Options.LogFileName;
+            listener.AutoExit = Options.AutoExit;
+
+            return listener;
+        }
     }
-
-    private static void StartListener()
-    {
-      _connectionListener = PrepareListener();
-      _connectionListener.Initialize();
-      _connectionListener.Start();
-    }
-
-    private static int StartAdbAndListener()
-    {
-      var handles = new ManualResetEvent[2];
-
-      var listenerHandle = new ManualResetEvent(false);
-      Action listenerAction = () =>
-      {
-        try
-        {
-          StartListener();
-        }
-        finally
-        {
-          listenerHandle.Set();
-        }
-      };
-
-      handles[0] = listenerHandle;
-      ThreadPool.QueueUserWorkItem(x => listenerAction());
-
-      var adbHandle = new ManualResetEvent(false);
-      Action adbAction = () =>
-      {
-        try
-        {
-          _adbActivtiyLaunchCommand = new AdbActivtiyLaunchCommand(Options.Activity, Options.AdbPath);
-          _adbActivtiyLaunchCommand.Execute();
-        }
-        finally
-        {
-          adbHandle.Set();
-        }
-      };
-      handles[1] = adbHandle;
-      ThreadPool.QueueUserWorkItem(x => adbAction());
-
-      WaitHandle.WaitAll(handles);
-
-      if (_connectionListener != null && _adbActivtiyLaunchCommand != null)
-      {
-        if (_connectionListener.ExitCode == 0 && _adbActivtiyLaunchCommand.ExitCode == 0)
-        {
-          return 0;
-        }
-      }
-
-      return -1;
-    }
-
-    public static void LogMessage(string message)
-    {
-      if (Options != null && Options.Verbose)
-      {
-        Console.WriteLine(message);
-      }
-    }
-
-    public static void LogMessage(string message, params object[] objects)
-    {
-      if (Options != null && Options.Verbose)
-      {
-        Console.WriteLine(message, objects);
-      }
-    }
-
-    private static SimpleConnectionListener PrepareListener()
-    {
-      var listener = new SimpleConnectionListener();
-
-      IPAddress ip;
-      if (String.IsNullOrEmpty(Options.IpAddress) || !IPAddress.TryParse(Options.IpAddress, out ip))
-      {
-        LogMessage("Failed to parse ip address. Using : " + IPAddress.Any);
-        listener.Address = IPAddress.Any;
-      }
-      else
-      {
-        listener.Address = ip;
-      }
-
-      ushort p;
-      if (UInt16.TryParse(Options.Port, out p))
-      {
-        listener.Port = p;
-      }
-      else
-      {
-        Console.WriteLine("Failed to parse port. Will use dafult.");
-      }
-
-      listener.LogPath = Options.LogFilePath ?? ".";
-      listener.LogFile = Options.LogFileName;
-      listener.AutoExit = Options.AutoExit;
-
-      return listener;
-    }
-  }
 }
